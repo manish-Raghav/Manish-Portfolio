7588d63b6438952c17ed0f56f9e1082992e77d84
Your Name <manishraghav95964@gmail.com>
2023-04-12 15:46:33 +0530

    Here i have Designed the Header Comp

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Component/Header/Header'
+import nav from './Component/Nav/nav'
+import About from './Component/About'
+import Exp from './Component/Expir/Exp'
+import Service from './Component/Services/Service'
+import Portfolio from './Component/Portfolio/Portfolio'
+import Testimonial from './Component/Testi/Testimonial'
+import contact from './Component/Context/contact'
+
+
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+     <Header />
+     <nav />
+     <About />
+     <Exp />
+     <Service/>
+     <Portfolio />
+     <Testimonial/>
+     <contact/>
+    </ >
   )
 }
 
---
+++ src/Component/Nav/Mynav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mynav = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Mynav
