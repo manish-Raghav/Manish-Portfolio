d52c7f2d2e8f182c8e4ae486737d4a4280819e03
Your Name <manishraghav95964@gmail.com>
2023-12-30 14:51:34 +0530

    30 Dec

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import './header.css'
  
  import myimg from '../../assets/manish.jpg'
- import {BsLinkedin} from 'react-icons/bs'
-import {ImGithub} from 'react-icons/Im'
-import {FaInstagramSquare} from 'react-icons/Fa'
+
+
 import Buttn from './Buttn'
-import Socials from './Socials'
+
 
  
 
